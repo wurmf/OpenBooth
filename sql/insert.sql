a3faeff7c8de9a47c79f805189e1df7762ecec87
Martin Schröder <ma.schroeder@outlook.com>
2016-11-25 11:12:09 +0100

    Changed data in image-insert

--- sql/insert.sql
+++ sql/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO profiles (name) VALUES ('Profile 2');
 INSERT INTO shootings (profileID,folderpath) VALUES (1,'/home/dummy/shooting1/');
 INSERT INTO shootings (profileID,folderpath) VALUES (2,'/home/dummy/shooting2/');
 
-INSERT INTO images VALUES ('/home/dummy/shooting1/img.jpg',1,'2003-06-25 16:50:20');
-INSERT INTO images VALUES ('/home/dummy/shooting1/img.jpg',2,'2003-06-25 16:50:20');
-INSERT INTO images VALUES ('/home/dummy/shooting1/img.jpg',1,'2003-06-25 16:50:20');
-INSERT INTO images VALUES ('/home/dummy/shooting1/img.jpg',2,'2003-06-25 16:50:20');
+INSERT INTO images VALUES ('/home/dummy/shooting1/img1.jpg',1,'2003-06-25 16:50:20');
+INSERT INTO images VALUES ('/home/dummy/shooting1/img2.jpg',1,'2003-06-25 16:50:20');
+INSERT INTO images VALUES ('/home/dummy/shooting2/img1.jpg',2,'2003-06-25 16:50:20');
+INSERT INTO images VALUES ('/home/dummy/shooting2/img2.jpg',2,'2003-06-25 16:50:20');
