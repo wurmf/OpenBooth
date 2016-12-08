eccab50852de8a7be9c660934ef33d8c2c21fce6
Radomir Andrejic <e8903383@student.tuwien.ac.at>
2016-12-08 20:16:58 +0100

    Neue Entities 'CameraPosition', 'Logo', 'LogoRposition' ins Git Repository aufgenommen

---
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/entities/CameraPosition.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.sepm.ws16.qse01.entities;
+
+/**
+ * Camera-Position-Entity
+ */
+public class CameraPosition {
+    private long profileId;
+    private Camera camera;
+    private Position position;
+    private boolean isGreenScreenReady;
+}
---
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/entities/LogoRposition.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.sepm.ws16.qse01.entities;
+
+/**
+ * Logo-Relative Position Entity
+ */
+public class LogoRposition {
+    private long profileId;
+    private Logo logo;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+}
