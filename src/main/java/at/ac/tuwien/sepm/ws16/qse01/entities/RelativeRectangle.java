ca9aa9ad3e7d72d7fa9c28a7d271b81298bfcc65
Radomir Andrejic <e8903383@student.tuwien.ac.at>
2016-12-10 00:07:51 +0100

    [Issue #11794] - Added RelativeRectangle entity class Implemented Constructor, Setter and Getter for all attributes

---
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/entities/RelativeRectangle.java
@@ -0,0 +1,53 @@
+package at.ac.tuwien.sepm.ws16.qse01.entities;
+
+/**
+ * Entity Relative Rectangle
+ * Inspired by java.awt.Rectangle Class
+ * It is intended to be used to define independent of the screen resolution a rectangle-shaped sub area
+ * at the same relative position and same relative size relative to the total screen
+ */
+public class RelativeRectangle {
+    private double x;
+    private double y;
+    private double height;
+    private double width;
+
+    public RelativeRectangle(double x, double y, double height, double width) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
