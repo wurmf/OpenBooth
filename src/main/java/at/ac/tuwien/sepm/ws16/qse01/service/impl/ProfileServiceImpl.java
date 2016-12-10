416ccf91be784efc1bf049938b97f764c2cea4fd
Radomir Andrejic <e8903383@student.tuwien.ac.at>
2016-12-10 11:23:28 +0100

    [Issue #11794] - Merge 'PositionServiceImpl' class into 'ProfileServiceImpl' class Move all 'PositionServiceImpl' method implementations into 'ProfileServiceImpl' class Adopt 'ProfileServiceImpl' constructor and Dependency Injection stuff

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/ProfileServiceImpl.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/ProfileServiceImpl.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.sepm.ws16.qse01.service.impl;
 
+import at.ac.tuwien.sepm.ws16.qse01.dao.PositionDAO;
 import at.ac.tuwien.sepm.ws16.qse01.dao.ProfileDAO;
 import at.ac.tuwien.sepm.ws16.qse01.dao.exceptions.PersistenceException;
+import at.ac.tuwien.sepm.ws16.qse01.entities.Position;
 import at.ac.tuwien.sepm.ws16.qse01.entities.Profile;
 import at.ac.tuwien.sepm.ws16.qse01.service.ProfileService;
 import at.ac.tuwien.sepm.ws16.qse01.service.exceptions.ServiceException;
@@ -22,15 +24,19 @@ public class ProfileServiceImpl implements ProfileService{
     private static final Logger LOGGER = LoggerFactory.getLogger(ProfileServiceImpl.class);
     @Resource
     private ProfileDAO profileDAO;
+    @Resource
+    private PositionDAO positionDAO;
     private List<Profile> profileList = new ArrayList<>();
+    private List<Position> positionList = new ArrayList<>();
 
     @Autowired
-    public ProfileServiceImpl(ProfileDAO profileDAO) throws ServiceException {
+    public ProfileServiceImpl(ProfileDAO profileDAO,PositionDAO positionDAO) throws ServiceException {
     //public ProfileServiceImpl() throws ServiceException {
         this.profileDAO = profileDAO;
+        this.positionDAO = positionDAO;
         try {
-            //profileDAO = new JDBCProfileDAO();
             profileList.addAll(profileDAO.readAll());
+            positionList.addAll(positionDAO.readAll());
         } catch (PersistenceException e) {
             throw new ServiceException("Error! Initializing service layer has failed.:" + e);
         }
@@ -38,7 +44,7 @@ public class ProfileServiceImpl implements ProfileService{
 
     @Override
     public Profile add(Profile profile) throws ServiceException {
-        LOGGER.debug("Entering add method with parameters " + profile);
+        LOGGER.debug("Entering addPosition method with parameters " + profile);
         try {
             Profile p = profileDAO.create(profile);
             profileList.add(p);
@@ -50,7 +56,7 @@ public class ProfileServiceImpl implements ProfileService{
 
     @Override
     public void edit(Profile profile) throws ServiceException {
-        LOGGER.debug("Entering edit method with parameters " + profile);
+        LOGGER.debug("Entering editPosition method with parameters " + profile);
         try {
             profileDAO.update(profile);
             this.profileList = profileDAO.readAll();
@@ -89,6 +95,43 @@ public class ProfileServiceImpl implements ProfileService{
         } catch (PersistenceException e) {
             throw new ServiceException("Error! Erasing in service layer has failed.:" + e);
         }
+    }
+
+    @Override
+    public Position addPosition(Position position) throws ServiceException {
+        LOGGER.debug("Entering addPosition method with parameters " + position);
+        try {
+            Position p = positionDAO.create(position);
+            positionList.add(p);
+            return p;
+        } catch (PersistenceException e) {
+            throw new ServiceException("Error! Adding in service layer has failed.:" + e);
+        }
+    }
+
+    public void editPosition(Position position) throws ServiceException {
+
+    }
+
+    @Override
+    public Position getPosition(int id) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public List<Position> getAllPositions() throws ServiceException {
+        LOGGER.debug("Entering getAllPositions method");
+        try {
+            this.positionList = positionDAO.readAll();
+            return this.positionList;
+        } catch (PersistenceException e) {
+            throw new ServiceException("Error! Getting All in service layer has failed.:" + e);
+        }
+    }
+
+    @Override
+    public void erasePosition(Position position) throws ServiceException {
+
     }
 
 }
