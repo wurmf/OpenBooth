98b7df27d3a200e1403d92a792adcb6720de72bb
Fabian Wurm <wurm.fabian@outlook.com>
2016-12-10 09:01:18 +0100

    [issue #12056]  start with implementation

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/LogoWatermarkServiceImpl.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/LogoWatermarkServiceImpl.java
@@ -1,8 +1,12 @@
 package at.ac.tuwien.sepm.ws16.qse01.service.impl;
 
 import at.ac.tuwien.sepm.ws16.qse01.service.LogoWatermarkService;
+import at.ac.tuwien.sepm.ws16.qse01.service.ProfileService;
 import at.ac.tuwien.sepm.ws16.qse01.service.exceptions.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 /**
  *
@@ -10,9 +14,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class LogoWatermarkServiceImpl implements LogoWatermarkService{
 
+    private ProfileService profileService;
+    private Map<String, Logo> cachedLogos = new HashMap<>();
+
+    @Autowired
+    public LogoWatermarkServiceImpl(ProfileService profileService){
+        this.profileService = profileService;
+    }
+
     @Override
     public void addLogosToImage(String srcImgPath) throws ServiceException {
-        
+
     }
 
     @Override
