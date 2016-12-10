38293f85214706692352b10306d66fd2703ff8d3
Fabian Wurm <wurm.fabian@outlook.com>
2016-12-10 12:34:31 +0100

    [issue #12056]  parts of service methods implemented

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/LogoWatermarkService.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/LogoWatermarkService.java
@@ -9,19 +9,28 @@ public interface LogoWatermarkService {
 
     /**
      * Adds the logos specified by the logo paths and logo positions given by the profile of the
-     * active shooting to the image specified by the given image path.
+     * active shooting to the image specified by the given image path und save it to the location specified
+     * by destImgPath
      * The logos are cached, so if the logo changes during a shooting it must have a different filename/path.
      *
-     * The original image will be overwritten.
+     * If the width or the height of a relative rectangle, representing a logo position is set to
+     * -1 it will be automatically scaled according to the given logo.
+     *
+     * If both width and length are set to -1 the absolute pixel values will be used
+     *
+     * If srcImgPath = destImgPath, the image located at srcImgPath will be overwritten.
+     * Example for destImgPath /home/fabian/shooting1/img1_with_logos.jpg
      * @param srcImgPath the image to which the logos will be added.
      * @throws ServiceException if an error during image processing, saving or opening occurs
      */
-     void addLogosToImage(String srcImgPath) throws ServiceException;
+     void addLogosCreateNewImage(String srcImgPath, String destImgPath) throws ServiceException;
 
     /**
-     * Adds a watermark to the image specified by the srcImgPath and saves it to the location specified by
-     * destImgPath.
-     * Example for destImgPath /home/fabian/shooting1/img_with_watermark.jpg
+     * Adds a watermark specified by the profile of the active shooting
+     * to the image specified by the srcImgPath and saves it to the location specified by destImgPath.
+     *
+     * If srcImgPath = destImgPath, the image located at srcImgPath will be overwritten.
+     * Example for destImgPath /home/fabian/shooting1/img1_with_watermark.jpg
      * @param srcImgPath specifies the image where the watermark should be added
      * @param destImgPath spefies the path and filename of the resulting image
      * @throws ServiceException if an error during the image processing, saving or opening occurs
--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/LogoWatermarkServiceImpl.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/LogoWatermarkServiceImpl.java
@@ -3,10 +3,19 @@ package at.ac.tuwien.sepm.ws16.qse01.service.impl;
 import at.ac.tuwien.sepm.ws16.qse01.service.LogoWatermarkService;
 import at.ac.tuwien.sepm.ws16.qse01.service.ProfileService;
 import at.ac.tuwien.sepm.ws16.qse01.service.exceptions.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -14,8 +23,12 @@ import java.util.HashMap;
 @Service
 public class LogoWatermarkServiceImpl implements LogoWatermarkService{
 
+    private static Logger LOGGER = LoggerFactory.getLogger(LogoWatermarkServiceImpl.class);
+
     private ProfileService profileService;
-    private Map<String, Logo> cachedLogos = new HashMap<>();
+    private Map<Logo, BufferedImage> cachedLogos = new HashMap<>();
+    private String currentWatermarkPath = null;
+    private BufferedImage cachedWatermarrk = null;
 
     @Autowired
     public LogoWatermarkServiceImpl(ProfileService profileService){
@@ -23,12 +36,88 @@ public class LogoWatermarkServiceImpl implements LogoWatermarkService{
     }
 
     @Override
-    public void addLogosToImage(String srcImgPath) throws ServiceException {
+    public void addLogosCreateNewImage(String srcImgPath, String destImgPath) throws ServiceException {
+
+        BufferedImage img;
+
+        try {
+            img = ImageIO.read(new File(srcImgPath));
+        } catch (IOException e) {
+            LOGGER.error("addLogosCreateNewImage - error loading given image" + e);
+            throw new ServiceException(e);
+        }
+
+        List<PairLogoRelativeRectangle> pairList = profileService.getListPairLogoRelativeRelativeRectangle();
+
+        //cache all Logos
+        for(pair : pairList){
+            Logo curLogo = pair.getLogo();
+            //comparison with equals in Logo
+            if(!cachedLogos.containsKey(curLogo)){
+                BufferedImage logoImage = ImageIO.read(curLogo.getPath());
+                cachedLogos.put(curLogo, logoImage);
+            }
+        }
+
+        for(pair : pairList){
+            BufferedImage curLogoImg = cachedLogos.get(pair.getLogo());
+            addLogoAtPosition(img, curLogoImg, pair.getRelativeRectangle());
+        }
+
+        try {
+            String formatName = destImgPath.substring(destImgPath.lastIndexOf('.'));
+            File newImage = new File(destImgPath);
+            ImageIO.write(img, formatName, newImage);
+        } catch (IOException e) {
+            LOGGER.error("addLogoCreateNewImage - error during saving new image" + e);
+            throw new ServiceException(e);
+        }
+
 
     }
 
     @Override
     public void addWatermarkCreateNewImage(String srcImgPath, String destImgPath) throws ServiceException {
 
+    }
+
+    private void addLogoAtPosition(BufferedImage img, BufferedImage logo, RelativeRectangle position){
+
+        double imgWidth = img.getWidth();
+        double imgHeight = img.getHeight();
+
+        double relativeXPosition = position.getX();
+        double relativeYPosition = position.getY();
+
+        int absoluteXPosition = (int)(imgWidth * relativeXPosition / (100d));
+        int absoluteYPosition = (int)(imgHeight * relativeYPosition / (100d));
+
+        double logoRelativeWidth = position.getWidth();
+        double logoRelativeHeight = position.getHeight();
+
+        LogoServiceImageObserver observer = new LogoServiceImageObserver(this);
+
+        Graphics g = img.getGraphics();
+
+        if(logoRelativeHeight == -1 && logoRelativeWidth == -1){
+            g.drawImage(logo, absoluteXPosition, absoluteYPosition, observer);
+            this.wait();
+        }else if(logoRelativeHeight == -1 && logoRelativeWidth > 0){
+            
+        }
+    }
+
+    private class LogoServiceImageObserver implements ImageObserver{
+        private LogoWatermarkService instanceToNotify;
+
+        private LogoServiceImageObserver(LogoWatermarkService instanceToNotify){
+            this.instanceToNotify = instanceToNotify;
+        }
+
+        @Override
+        public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+            instanceToNotify.notify();
+            return false;
+        }
     }
 }
