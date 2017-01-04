13ffaff301c85abe11e43f53e87bb875db05926d
Martin Schröder <ma.schroeder@outlook.com>
2017-01-04 11:57:18 +0100

    Deleted unused Model

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/gui/model/SelectedImageShareModel.java
+++
@@ -1,8 +0,0 @@
-package at.ac.tuwien.sepm.ws16.qse01.gui.model;
-
-/**
- * Used by MiniaturFrameController and FullScreenImageController to share communicate which image is selected.
- */
-public interface SelectedImageShareModel {
-    //TODO: define methods
-}
