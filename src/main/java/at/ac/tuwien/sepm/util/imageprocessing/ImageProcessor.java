48b98a216adc5b28622c4390f8d8b20442ef38c9
Fabian Wurm <wurm.fabian@outlook.com>
2017-01-12 22:42:07 +0100

    [Issue #12630]Structure of ImageProcessor implemented

--- src/main/java/at/ac/tuwien/sepm/util/imageprocessing/ImageProcessor.java
+++ src/main/java/at/ac/tuwien/sepm/util/imageprocessing/ImageProcessor.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.sepm.util.imageprocessing;
 
+import at.ac.tuwien.sepm.ws16.qse01.entities.Image;
+import at.ac.tuwien.sepm.ws16.qse01.service.exceptions.ServiceException;
+
 /**
  * This class provides methods for processing previews and shots
  */
@@ -11,7 +14,7 @@ public interface ImageProcessor {
      * The ShotFrame is set by the implementation
      * @param imgPath must specify a valid image
      */
-    void processPreview(String imgPath);
+    void processPreview(String imgPath) throws ServiceException;
 
     /**
      * Processes a "real" shot/picture.
@@ -19,7 +22,7 @@ public interface ImageProcessor {
      * by using the ImageService. MiniaturFrame and FullScreenFrame will be refreshed and the
      * image will be passed to the correct ShotFrame
      * The ShotFrame is set by the implementation
-     * @param imgPath
+     * @param image must specify a valid image
      */
-    void processShot(String imgPath);
+    void processShot(Image image) throws ServiceException;
 }
