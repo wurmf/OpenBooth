d8955f1080396cef06658a5a8f098ee6efe5450a
Fabian Wurm <wurm.fabian@outlook.com>
2017-01-12 10:09:16 +0100

    ImageHelper Dokumentation added

--- src/main/java/at/ac/tuwien/sepm/util/ImageHelper.java
+++ src/main/java/at/ac/tuwien/sepm/util/ImageHelper.java
@@ -26,13 +26,20 @@ public class ImageHelper {
 
     private final List<String> supportedImageFormats = Arrays.asList("jpg", "jpeg", "bmp", "png");
 
-    public ImageHelper(OpenCVLoader openCVLoader) throws ServiceException{
+    public ImageHelper(OpenCVLoader openCVLoader) throws ServiceException {
         openCVLoader.loadLibrary();
     }
 
-    public BufferedImage openImage(String imagePath) throws ServiceException{
+    /**
+     * Opens the image specified by imagePath as BufferedImage
+     *
+     * @param imagePath thi given path, must specify a valid image
+     * @return the given image opened as BufferedImage
+     * @throws ServiceException if an error during the file opening occurs
+     */
+    public BufferedImage openImage(String imagePath) throws ServiceException {
 
-        if(imagePath == null || imagePath.isEmpty()){
+        if (imagePath == null || imagePath.isEmpty()) {
             LOGGER.error("openImage - imagePath is null or empty");
             throw new ServiceException("imagePath is null or empty");
         }
@@ -42,58 +49,78 @@ public class ImageHelper {
         try {
             img = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
-            LOGGER.error("openImage - error loading given image " , e);
+            LOGGER.error("openImage - error loading given image ", e);
             throw new ServiceException(e);
         }
         LOGGER.debug("Image at {} opened", imagePath);
         return img;
     }
 
-    public void saveImage(BufferedImage image, String destPath) throws ServiceException{
-        if(destPath == null || destPath.isEmpty()){
+    /**
+     * Saves the given BufferedImage to the location specified by destPath
+     * <p>
+     * Example of a valid image path: /home/fabian/image1.jpg
+     * <p>
+     * Supported file formats: jpg, bmp, png
+     *
+     * @param image    th given image, must specify a valid image
+     * @param destPath the given destination path, must specify a valid image path including the filename and ending
+     * @throws ServiceException when an error during the image saving occurs
+     */
+    public void saveImage(BufferedImage image, String destPath) throws ServiceException {
+        if (destPath == null || destPath.isEmpty()) {
             LOGGER.error("saveImage - destPath is null or empty");
             throw new ServiceException("destPath is null or empty");
         }
 
         try {
             String formatName = destPath.substring(destPath.lastIndexOf('.') + 1);
-            if(!supportedImageFormats.contains(formatName)){
+            if (!supportedImageFormats.contains(formatName)) {
                 LOGGER.error("Image format {} not supported", formatName);
                 throw new ServiceException("Image format not supported");
             }
             File newImage = new File(destPath);
             ImageIO.write(image, formatName, newImage);
         } catch (IOException e) {
-            LOGGER.error("saveImage - error during saving image - " , e);
+            LOGGER.error("saveImage - error during saving image - ", e);
             throw new ServiceException(e);
         }
 
         LOGGER.debug("Image saved to {}", destPath);
     }
 
-    public BufferedImage convertMatToBufferedImg(Mat srcMat) throws ServiceException{
+    /**
+     * Converts a given opencv Mat to a BufferedImage
+     * The Mat must have either CV_8UC1 or CV_8UC3 as type (Grayscale or 3 Byte unsigned int as color)
+     * The given Mat can be released afterwards if necessary
+     *
+     * @param srcMat the given opencv Mat
+     * @return the converted BufferedImage
+     * @throws ServiceException when an error during the conversion occurs
+     */
+    public BufferedImage convertMatToBufferedImg(Mat srcMat) throws ServiceException {
 
-        if(srcMat == null){
+        if (srcMat == null) {
             LOGGER.error("convertMatToBufferedImg - given srcMat is null");
             throw new ServiceException("Given Mat is null");
         }
 
         int bufferedImgType;
 
-        if(srcMat.type() == CvType.CV_8UC3){
+        if (srcMat.type() == CvType.CV_8UC3) {
             bufferedImgType = BufferedImage.TYPE_3BYTE_BGR;
-        } else if (srcMat.type() == CvType.CV_8UC1){
+        } else if (srcMat.type() == CvType.CV_8UC1) {
             bufferedImgType = BufferedImage.TYPE_BYTE_GRAY;
-        }else {
+        } else {
             LOGGER.error("convertMatToBufferedImg - color type must be CV_8UC3 or CV_8UCM but was {}", srcMat.type());
             throw new ServiceException("could not convert Mat to BufferedImage");
         }
 
-        byte[] data = new byte[srcMat.rows() * srcMat.cols() * (int)(srcMat.elemSize())];
+        byte[] data = new byte[srcMat.rows() * srcMat.cols() * (int) (srcMat.elemSize())];
         srcMat.get(0, 0, data);
-        BufferedImage result = new BufferedImage(srcMat.cols(),srcMat.rows(), bufferedImgType);
+        BufferedImage result = new BufferedImage(srcMat.cols(), srcMat.rows(), bufferedImgType);
 
-        if(bufferedImgType == BufferedImage.TYPE_3BYTE_BGR) {
+        if (bufferedImgType == BufferedImage.TYPE_3BYTE_BGR) {
 
             //Color Ordering for Mat is BGR but for BufferedImage is must be RGB so color ordering is changed
             byte b;
@@ -110,6 +137,13 @@ public class ImageHelper {
         return result;
     }
 
+    /**
+     * Converts a given BufferedImage to an opencv Mat
+     * The BufferedImage must have type TYPE_3BYTE_BGR
+     * @param srcImg the given BufferedImage
+     * @return the converted Mat (Type CV_8UC3)
+     * @throws ServiceException when an error during the conversion occurs
+     */
     public Mat convertBufferedImgToMat(BufferedImage srcImg) throws ServiceException{
 
         if(srcImg == null){
