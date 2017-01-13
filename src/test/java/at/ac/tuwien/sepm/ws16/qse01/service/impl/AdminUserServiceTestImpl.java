88714d5a23803c4a9ba009c71063f37fbadeab33
Martin Schröder <ma.schroeder@outlook.com>
2017-01-13 20:50:40 +0100

    Neue Exception hinzugefügt

--- src/test/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/AdminUserServiceTestImpl.java
+++ src/test/java/at/ac/tuwien/sepm/ws16/qse01/service/impl/AdminUserServiceTestImpl.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepm.ws16.qse01.service.impl;
 
 import at.ac.tuwien.sepm.util.dbhandler.impl.H2EmbeddedHandler;
+import at.ac.tuwien.sepm.util.exceptions.DatabaseException;
 import at.ac.tuwien.sepm.ws16.qse01.dao.exceptions.PersistenceException;
 import at.ac.tuwien.sepm.ws16.qse01.dao.impl.JDBCAdminUserDAO;
 import at.ac.tuwien.sepm.ws16.qse01.service.AbstractAdminUserServiceTest;
@@ -12,7 +13,7 @@ import org.junit.Before;
  */
 public class AdminUserServiceTestImpl extends AbstractAdminUserServiceTest{
     @Before
-    public void setUp() throws PersistenceException{
+    public void setUp() throws PersistenceException, DatabaseException{
         H2EmbeddedHandler.getInstance().getTestConnection();
         setAdminUserService(new AdminUserServiceImpl(new JDBCAdminUserDAO(H2EmbeddedHandler.getInstance())));
     }
