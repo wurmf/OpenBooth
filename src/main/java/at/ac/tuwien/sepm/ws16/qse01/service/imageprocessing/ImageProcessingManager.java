ba680c7d8decccba65b00538dd026a9f47e5c76e
Fabian Wurm <wurm.fabian@outlook.com>
2017-01-14 16:16:03 +0100

    ImageProcessingManager and ImageProcessor adapted for changes in ShotFrameManager

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/imageprocessing/ImageProcessingManager.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/service/imageprocessing/ImageProcessingManager.java
@@ -3,6 +3,8 @@ package at.ac.tuwien.sepm.ws16.qse01.service.imageprocessing;
 import at.ac.tuwien.sepm.util.ImageHandler;
 import at.ac.tuwien.sepm.util.OpenCVLoader;
 import at.ac.tuwien.sepm.util.exceptions.LibraryLoadingException;
+import at.ac.tuwien.sepm.ws16.qse01.entities.Position;
+import at.ac.tuwien.sepm.ws16.qse01.gui.ShotFrameController;
 import at.ac.tuwien.sepm.ws16.qse01.service.imageprocessing.impl.ImageProcessorImpl;
 import at.ac.tuwien.sepm.ws16.qse01.application.ShotFrameManager;
 import at.ac.tuwien.sepm.ws16.qse01.camera.CameraHandler;
@@ -17,7 +19,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by fabian on 13.01.17.
@@ -53,14 +57,21 @@ public class ImageProcessingManager {
     public void initImageProcessing() throws ServiceException{
         LOGGER.debug("entering initImageProcessing method ");
         List<Camera> cameraList = cameraHandler.getCameras();
-        shotFrameManager.init(cameraList);
+        List<Position> positionList = new ArrayList<>();
+        for(Camera c : cameraList){
+            Position p = profileService.getPositionOfCameraOfProfile(c);
+            positionList.add(p);
+        }
+
+        Map<Position, ShotFrameController> positionShotFrameMap = shotFrameManager.init(positionList);
 
 
         cameraThreadList = cameraHandler.createThreads();
         for(CameraThread cameraThread : cameraThreadList){
-            Camera camera  = cameraThread.getCamera();
+            Position position  = profileService.getPositionOfCameraOfProfile(cameraThread.getCamera());
+            ShotFrameController shotFrameController = positionShotFrameMap.get(position);
 
-            ImageHandler imageHandler = null;
+            ImageHandler imageHandler;
             try {
                 imageHandler = new ImageHandler(openCVLoader);
             } catch (LibraryLoadingException e) {
@@ -71,7 +82,7 @@ public class ImageProcessingManager {
             LogoWatermarkService logoWatermarkService = new LogoWatermarkServiceImpl(profileService, imageHandler);
             FilterService filterService = new FilterServiceImpl(shootingService, openCVLoader, imageHandler);
 
-            ImageProcessor imageProcessor = new ImageProcessorImpl(shotFrameManager, shootingService, profileService, imageService, logoWatermarkService, filterService, camera, imageHandler, refreshManager);
+            ImageProcessor imageProcessor = new ImageProcessorImpl(shotFrameController, shootingService, profileService, imageService, logoWatermarkService, filterService, position, imageHandler, refreshManager);
 
             cameraThread.setImageService(imageService);
             cameraThread.setShootingService(shootingService);
