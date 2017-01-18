ef09b97257839c902ecf9811bb96c169f96e0f88
Aniela <e1456160@student.tuwien.ac.at>
2017-01-18 11:29:27 +0100

    merge def into aniela

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/gui/ShootingAdminController.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/gui/ShootingAdminController.java
@@ -166,14 +166,14 @@ public class ShootingAdminController {
 
                     shootingService.addShooting(shouting);
 
-                    windowManager.showScene(WindowManager.SHOW_CUSTOMERSCENE);
+                   // windowManager.showScene(WindowManager.SHOW_CUSTOMERSCENE);
 
-                 /* boolean camerasFitPosition =windowManager.initImageProcessing();
+                  boolean camerasFitPosition =windowManager.initImageProcessing();
                     if(camerasFitPosition){
                         windowManager.showScene(WindowManager.SHOW_CUSTOMERSCENE);
                     } else{
                         showInformationDialog("Wählen sie ein anderes Profil das zu ihrem Kamerasetup passt.");
-                    }*/
+                    }
                 } catch (ServiceException serviceExeption) {
                     LOGGER.error("onStartShootingPressed - ", serviceExeption);
                     showInformationDialog("Es konnte kein Shooting erstellt werden.");
