1420b05aff57cb2183882b77cab5fcc9307e643f
Fabian Wurm <wurm.fabian@outlook.com>
2017-01-24 13:21:38 +0100

    filter preview images copy out of ressource folder

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/gui/CameraFilterController.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/gui/CameraFilterController.java
@@ -149,7 +149,12 @@ public class CameraFilterController {
                 filtermap.clear();
             }
             if(filtermap==null||filtermap.isEmpty()){
-                filtermap = filterService.getAllFilteredImages(System.getProperty("user.dir") + "/src/main/resources/images/filterPreview.png");
+                try {
+                    String filterPreviewImagePath = wm.copyResource("/images/filterPreview.png");
+                    filtermap = filterService.getAllFilteredImages(filterPreviewImagePath);
+                } catch (IOException e) {
+                    LOGGER.error("createButtons - could not copy preview filter image", e);
+                }
             }
            // List<Image> imlist= imageService.getAllImages(shootingService.searchIsActive().getId());
             for (Map.Entry<String, BufferedImage> filterentety: filtermap.entrySet()) {//imagefilter.size
