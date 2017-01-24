89aaba2ec3b4d4b0c7a5d83a6e14890861541f24
Benjamin Letz <e1425601@student.tuwien.ac.at>
2017-01-24 13:41:03 +0100

    initializing closed cameras

--- src/main/java/at/ac/tuwien/sepm/ws16/qse01/camera/impl/CameraHandlerImpl.java
+++ src/main/java/at/ac/tuwien/sepm/ws16/qse01/camera/impl/CameraHandlerImpl.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepm.ws16.qse01.camera.impl;
 import at.ac.tuwien.sepm.ws16.qse01.camera.CameraHandler;
 import at.ac.tuwien.sepm.ws16.qse01.camera.exeptions.CameraException;
 
+import at.ac.tuwien.sepm.ws16.qse01.camera.libgphoto2java.CameraFile;
 import at.ac.tuwien.sepm.ws16.qse01.camera.libgphoto2java.CameraGphoto;
 import at.ac.tuwien.sepm.ws16.qse01.camera.libgphoto2java.CameraList;
 import at.ac.tuwien.sepm.ws16.qse01.camera.libgphoto2java.CameraUtils;
@@ -60,9 +61,17 @@ public class CameraHandlerImpl implements CameraHandler {
         return threadList;
     }
 
+    @Override
     public List<Camera> getCameras() throws CameraException {
         if(!cameraList.isEmpty())
         {
+            for (CameraGphoto camera: cameraGphotoList)
+            {
+                if(!camera.isInitialized())
+                {
+                    camera.initialize();
+                }
+            }
             return cameraList;
             /*
             for (CameraGphoto camera: cameraGphotoList)
