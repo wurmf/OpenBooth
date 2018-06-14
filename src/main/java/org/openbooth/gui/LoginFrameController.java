eaf00e204825c2089304d19fcfbd8345254a5293
Martin Schröder <ma.schroeder@outlook.com>
2018-06-14 18:25:38 +0200

    [Issue #28] Minor cleanup

--- src/main/java/org/openbooth/gui/LoginFrameController.java
+++ src/main/java/org/openbooth/gui/LoginFrameController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -18,8 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.awt.event.KeyListener;
-import java.security.Key;
 
 @Component
 public class LoginFrameController {
