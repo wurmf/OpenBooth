b47ad145eb07d0fac59640011729cb8b855d078f
Fabian Wurm <wurm.fabian@outlook.com>
2018-11-13 18:32:07 +0100

    [#67] Implemented GPhotoCameraHandler with support for exactly one camera

--- src/main/java/org/openbooth/camera/impl/SimCameraHandler.java
+++ src/main/java/org/openbooth/camera/impl/SimCameraHandler.java
@@ -3,11 +3,13 @@ package org.openbooth.camera.impl;
 import org.openbooth.camera.CameraHandler;
 import org.openbooth.camera.exceptions.CameraException;
 import org.openbooth.util.FileTransfer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Profile("simulated_camera")
 public class SimCameraHandler implements CameraHandler {
 
     private static final String SIM_PREVIEW_IMAGE_PATH = "/images/simcam/sim_preview_image.png";
@@ -21,6 +23,11 @@ public class SimCameraHandler implements CameraHandler {
     @Override
     public String captureAndTransferPreviewFromCamera(String storagePath, String imageName) throws CameraException {
         return transferImageToLocation(SIM_PREVIEW_IMAGE_PATH, storagePath, imageName);
+    }
+
+    @Override
+    public void initialize(){
+        //This is supposed to be empty, the SimCameraHandler doesn't need initialization
     }
 
     private String transferImageToLocation(String originalImagePath, String storagePath, String imageName) throws CameraException{
